433822d58c7d64ba1fc46fe712872b5f4ba17cae
Darkhan Zhursin <daratechn@gmail.com>
2024-08-13 08:40:41 -0500

    DI

--- src/main/java/framework/FWContext.java
+++ src/main/java/framework/FWContext.java
@@ -2,6 +2,7 @@ package framework;
 
 import org.reflections.Reflections;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,7 @@ public class FWContext {
         try {
             Reflections reflections = new Reflections(clazz.getPackageName());
             scannAndInstatiateServiceClasses(reflections);
+            performDI();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -26,5 +28,46 @@ public class FWContext {
         for (Class<?> serviceClass : servicetypes) {
             serviceObjectList.add((Object) serviceClass.getDeclaredConstructor().newInstance());
         }
+    }
+
+    private void performDI() {
+        try {
+            for (Object serviceObject : serviceObjectList) {
+                // for fields
+                for (Field field : serviceObject.getClass().getDeclaredFields()) {
+                    if (field.isAnnotationPresent(Autowired.class)) {
+                        // get the type of the field
+                        Class<?> theFieldType =field.getType();
+                        //get the object instance of this type
+                        Object instance = getServiceBeanOftype(theFieldType);
+                        //do the injection
+                        field.setAccessible(true);
+                        field.set(serviceObject, instance);
+                    }
+                }
+                // TODO
+                //for methods
+                //for constructors
+            }
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Object getServiceBeanOftype(Class interfaceClass) {
+        Object service = null;
+        try {
+            for (Object theClass : serviceObjectList) {
+                Class<?>[] interfaces = theClass.getClass().getInterfaces();
+
+                for (Class<?> theInterface : interfaces) {
+                    if (theInterface.getName().contentEquals(interfaceClass.getName()))
+                        service = theClass;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return service;
     }
 }
