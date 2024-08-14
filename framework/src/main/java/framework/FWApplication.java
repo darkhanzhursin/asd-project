b49fdeac8b9ac2d707e7c26d4a24988dea9a3ff0
Darkhan Zhursin <daratechn@gmail.com>
2024-08-13 20:59:09 -0500

    minor fix

--- framework/src/main/java/framework/FWApplication.java
+++ framework/src/main/java/framework/FWApplication.java
@@ -8,27 +8,5 @@ public class FWApplication {
         // create the context
         FWContext fwContext = new FWContext();
         fwContext.start(applicationClass);
-        try {
-//            // create instance of the application class
-            Object applicationObject = applicationClass.getDeclaredConstructor().newInstance();
-            // find annotated fields
-            for (Field field : applicationObject.getClass().getDeclaredFields()) {
-                if (field.isAnnotationPresent(Autowired.class)) {
-                    // get the type of the field
-                    Class<?> fieldType = field.getType();
-                    // get the object instance of this type
-                    Object instance = fwContext.getServiceBeanOfType(fieldType);
-                    // do the injection
-                    field.setAccessible(true);
-                    field.set(applicationObject, instance);
-                }
-            }
-            // call the run() method
-            if (applicationObject instanceof Runnable) {
-                ((Runnable) applicationObject).run();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
--- framework/src/main/java/framework/FWContext.java
+++ framework/src/main/java/framework/FWContext.java
@@ -4,39 +4,21 @@ import org.reflections.Reflections;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 public class FWContext {
 
-    private static List<Object> componentObjectList = new ArrayList<>();
     private static List<Object> serviceObjectList = new ArrayList<>();
 
     public void start(Class<?> clazz) {
         try {
             Reflections reflections = new Reflections(clazz.getPackageName());
             scannAndInstatiateServiceClasses(reflections);
-            //scannAndInstatiateComponentClasses(reflections);
-//            performDI();
-
-//            for (Object serviceObject : componentObjectList) {
-//                for (Method method : serviceObject.getClass().getDeclaredMethods()) {
-//                    method.invoke(serviceObject);
-//                }
-//            }
+            performDI(clazz);
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-
-    private void scannAndInstatiateComponentClasses(Reflections reflections) throws InstantiationException,
-            IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        // find and instantiate all classes annotated with the @Component annotation
-        Set<Class<?>> types = reflections.getTypesAnnotatedWith(Component.class);
-        for (Class<?> implementationClass : types) {
-            componentObjectList.add(implementationClass.getDeclaredConstructor().newInstance());
         }
     }
 
@@ -48,27 +30,28 @@ public class FWContext {
         }
     }
 
-    private void performDI() {
+    private void performDI(Class<?> applicationClass) {
         try {
-            for (Object componentObject : componentObjectList) {
-                // for fields
-                for (Field field : componentObject.getClass().getDeclaredFields()) {
-                    if (field.isAnnotationPresent(Autowired.class)) {
-                        // get the type of the field
-                        Class<?> theFieldType =field.getType();
-                        //get the object instance of this type
-                        Object instance = getServiceBeanOfType(theFieldType);
-                        //do the injection
-                        field.setAccessible(true);
-                        field.set(componentObject, instance);
-                    }
+//            // create instance of the application class
+            Object applicationObject = applicationClass.getDeclaredConstructor().newInstance();
+            // find annotated fields
+            for (Field field : applicationObject.getClass().getDeclaredFields()) {
+                if (field.isAnnotationPresent(Autowired.class)) {
+                    // get the type of the field
+                    Class<?> fieldType = field.getType();
+                    // get the object instance of this type
+                    Object instance = getServiceBeanOfType(fieldType);
+                    // do the injection
+                    field.setAccessible(true);
+                    field.set(applicationObject, instance);
                 }
-                // TODO
-                //for methods
-                //for constructors
             }
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
+            // call the run() method
+            if (applicationObject instanceof Runnable) {
+                ((Runnable) applicationObject).run();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
